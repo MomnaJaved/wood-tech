cd643c13a3835d82b6e660df462888bfc02c22e9
Zarnab Imran <zarnab@gigalabs.co>
2025-05-22 19:38:38 +0500

    changes

--- src/components/TrustedCard.tsx
+++ src/components/TrustedCard.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-interface PartnerProps {
-  partner: {
-    id: number;
-    image: string;
-    slug: string;
-  };
-}
+type Partner = {
+  id: number;
+  image: string;
+  slug: string;
+};
 
-const TrustedCard: React.FC<PartnerProps> = ({ partner }) => {
+type PartnerProps = {
+  partner: Partner;
+};
+
+const TrustedCard = ({ partner }: PartnerProps) => {
   return (
     <Link
       to={`/chooseUS/${partner.slug}`}
@@ -17,22 +19,22 @@ const TrustedCard: React.FC<PartnerProps> = ({ partner }) => {
     >
       <div
         className="
-    bg-white rounded-lg 
-    shadow-[0_0_8px_2px_rgba(255,255,255,0.8)] 
-    hover:shadow-none
-    p-2 h-[35vh] w-[18vw] 
-    flex flex-col items-start text-left
-    transition-shadow duration-300 mt-15 
-  "
+          bg-white rounded-lg 
+          shadow-[0_0_8px_2px_rgba(255,255,255,0.8)] 
+          hover:shadow-none
+          p-2 h-[35vh] w-[18vw] 
+          flex flex-col items-start text-left
+          transition-shadow duration-300 mt-15 
+        "
       >
         <img
           src={partner.image}
           className="
-      mb-4 w-full h-auto 
-      transition-transform duration-300
-      hover:scale-110 hover:-translate-x-2
-      cursor-pointer
-    "
+            mb-4 w-full h-auto 
+            transition-transform duration-300
+            hover:scale-110 hover:-translate-x-2
+            cursor-pointer
+          "
         />
       </div>
     </Link>
